d4d8a82adccfae68790abf93eec2c777f6bece24
Gonzalo <Fernandez-gonza@hotmail.com>
2024-04-20 18:17:34 -0300

    add: model Restaurant

--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -4,6 +4,9 @@ const databaseConnection = async () => {
     try {
         await sequelize.authenticate();
         console.log("Connection has been established successfully.");
+
+        await sequelize.sync({ alter: true });
+        console.log("All models were synchronized successfully.");
     } catch (error) {
         console.error("Unable to connect to the database:", error);
     }
---
+++ src/models/Restaurant.ts
@@ -0,0 +1,58 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../db/dbConfig';
+
+interface RestaurantAttributes {
+    id: number;
+    rating: number;
+    name: string;
+    site: string;
+    email: string;
+    phone: string;
+    street: string;
+    city: string;
+    state: string;
+    lat: number;
+    lng: number;
+}
+
+class Restaurant extends Model<RestaurantAttributes> implements RestaurantAttributes {
+    public id!: number;
+    public rating!: number;
+    public name!: string;
+    public site!: string;
+    public email!: string;
+    public phone!: string;
+    public street!: string;
+    public city!: string;
+    public state!: string;
+    public lat!: number;
+    public lng!: number;
+}
+
+Restaurant.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        rating: DataTypes.INTEGER,
+        name: DataTypes.TEXT,
+        site: DataTypes.TEXT,
+        email: DataTypes.TEXT,
+        phone: DataTypes.TEXT,
+        street: DataTypes.TEXT,
+        city: DataTypes.TEXT,
+        state: DataTypes.TEXT,
+        lat: DataTypes.FLOAT,
+        lng: DataTypes.FLOAT,
+    },
+    {
+        sequelize,
+        modelName: 'Restaurant',
+        tableName: 'Restaurants',
+        timestamps: false, //enable/disable fields timestamp (createdAt y updatedAt)
+    }
+);
+
+export { Restaurant };
